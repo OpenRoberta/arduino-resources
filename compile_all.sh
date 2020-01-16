839d2ad450f7160087d24b6f3cd3aac8a778eb92
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2020-01-16 14:42:04 +0100

    issue OpenRoberta/openroberta-lab#547 - copying includes now

--- compile_all.sh
+++ compile_all.sh
@@ -7,3 +7,7 @@ mkdir -p /tmp/ora-res
 cp -r release /opt/ora-res
 mv release RobotArdu
 rm -rf target 
+
+mkdir -p ./RobotArdu/release/includes
+cd ./RobotArdu/libraries
+find . -name '*.h' -exec cp --parents \{\} ../release/includes \;
