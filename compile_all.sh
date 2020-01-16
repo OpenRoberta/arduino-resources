de321a5c6c0f3656dcbfe92f88359044f5fb4e8b
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2020-01-16 13:45:24 +0100

    issue OpenRoberta/openroberta-lab#547 - changed the way the container builds the resources

--- compile_all.sh
+++ compile_all.sh
@@ -3,5 +3,7 @@
 ./compile_resources.sh uno
 ./compile_resources.sh nano :cpu=atmega328
 ./compile_resources.sh mega :cpu=atmega2560
+mkdir -p /tmp/ora-res
+cp -r release /opt/ora-res
 mv release RobotArdu
 rm -rf target 
