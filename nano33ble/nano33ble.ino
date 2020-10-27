469294adcb61f8b93c1528e66a064dfe9adaf1f1
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2020-10-27 14:12:05 +0100

    add nano 33 ble sense libraries

--- nano33ble/nano33ble.ino
+++ nano33ble/nano33ble.ino
@@ -1,9 +1,10 @@
 #define _ARDUINO_STL_NOT_NEEDED
 #include <Arduino.h>
 #include <NEPODefs.h>
-#include <LiquidCrystal.h>
-#include <LiquidCrystal_I2C.h>
-#include <Stepper.h>
+#include <Arduino_LSM9DS1.h>
+#include <Arduino_APDS9960.h>
+#include <Arduino_LPS22HB.h>
+#include <Arduino_HTS221.h>
 
 void setup()
 {
